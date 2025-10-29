5377eb6cf05aef167bdc3a547325b99a476f996a
basheerabegum2001 <basheerabegum276@gmail.com>
2025-10-29 17:13:39 +0530

    Edited dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -13,17 +13,59 @@ if (!isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        .welcome{
+            margin: 30px;
+            text-align: center; 
+            color: purple;
+            font-size: 40px;
+        }
+        .btn-additem, .btn-view {
+        display: inline-block;
+        padding: 6px 12px;
+        margin: 8px;
+        text-decoration: none;
+        color: white;
+        border-radius: 4px;
+        font-size: 14px;
+        transition: background 0.3s;
+        }
+        .btn-additem {
+        background-color:antiquewhite ;
+        }
+
+        .btn-additem:hover {
+        background-color:white ;
+        }
+        .btn-view {
+        background-color:antiquewhite ;
+        }
+
+        .btn-view:hover {
+        background-color:white;
+        }
+        .logbtn {
+        background-color:rgb(255, 0, 0) ;
+        margin-left: 1350px;
+        padding: 8px;
+        }
+
+        .logbtn:hover {
+        background-color:rgba(250, 235, 215, 0.89) ;
+        }
+
+    </style>
 </head>
 <body>
     <div>
        <button type="submit" class="logbtn"><a href="logout.php">Logout</a></button>
     </div>
 
-<div style="text-align:center";>
+<div class = "welcome">
  <h1 style="margin:80px 30px";>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
 
-<button type="submit" style="margin-left:20px";><a href="add_item.php">Add Item</a> </button> 
-<button type="submit"  style="margin-left:40px";><a href="view_items.php">View Items</a>  </button>
+<button type="submit" class="btn-additem" style="margin-left:20px";><a href="add_item.php">Add Item</a> </button> 
+<button type="submit"  class="btn-view" style="margin-left:40px";><a href="view_items.php">View Items</a>  </button>
 </div>
 
 </body>
